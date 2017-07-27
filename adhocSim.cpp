d36108ed88e64270dd5f0ef0bd09e6967b11e998
William Wu <will.go.code@gmail.com>
2017-07-27 17:12:39 -0400

    Removed loop as it didn't randomize anything past the first run.

--- adhocSim.cpp
+++ adhocSim.cpp
@@ -13,47 +13,45 @@ int main(int argc, char *argv[]) {
 	if(argc != 3) { cout << "./adhocSim.exe <numberOfNodes> <sizeOfMatrix>" << endl; return 0;}
 
 	cout << endl;
-	for(int i = 0; i < 20; i++) {
-		int srcX = 0, srcY = 0, destX = 0, destY = 0;
-		int numberOfNodes = stoi(argv[1]);
-		int matrixSize = stoi(argv[2]);
-		srand((int)time(0));
-		map<tuple<int, int>, Node* > nodeMap;
-		
-		generateNodes(&nodeMap, numberOfNodes, matrixSize);
-		createSrcAndDestination(&nodeMap, srcX, srcY, destX, destY, numberOfNodes);
-		bool found = findPath(nodeMap, srcX, srcY, destX, destY, numberOfNodes);
-		if(found) { printPath(nodeMap, destX, destY); }
+	int srcX = 0, srcY = 0, destX = 0, destY = 0;
+	int numberOfNodes = stoi(argv[1]);
+	int matrixSize = stoi(argv[2]);
+	srand((int)time(0));
+	map<tuple<int, int>, Node* > nodeMap;
+	
+	generateNodes(&nodeMap, numberOfNodes, matrixSize);
+	createSrcAndDestination(&nodeMap, srcX, srcY, destX, destY, numberOfNodes);
+	bool found = findPath(nodeMap, srcX, srcY, destX, destY, numberOfNodes);
+	if(found) { printPath(nodeMap, destX, destY); }
 
-		/*
-		int nodeMatrix[matrixSize][matrixSize];
-		for(int i = 0; i < matrixSize; i++) {
-			for(int j = 0; j < matrixSize; j++) {
-				nodeMatrix[i][j] = 0;
-			}
+	/*
+	int nodeMatrix[matrixSize][matrixSize];
+	for(int i = 0; i < matrixSize; i++) {
+		for(int j = 0; j < matrixSize; j++) {
+			nodeMatrix[i][j] = 0;
 		}
-		
-		// set up matrix
-		for(auto it = nodeMap.begin(); it != nodeMap.end(); it++) {
-			auto coordinates = it -> first;
-			nodeMatrix[get<0>(coordinates)][get<1>(coordinates)] += 1;
-		}
-		nodeMatrix[srcX][srcY]++;
-		nodeMatrix[destX][destY] += 2;
-		// end set up matrix 
+	}
+	
+	// set up matrix
+	for(auto it = nodeMap.begin(); it != nodeMap.end(); it++) {
+		auto coordinates = it -> first;
+		nodeMatrix[get<0>(coordinates)][get<1>(coordinates)] += 1;
+	}
+	nodeMatrix[srcX][srcY]++;
+	nodeMatrix[destX][destY] += 2;
+	// end set up matrix 
 
-		// print matrix 
-		for(int i = 0; i < matrixSize; i++) {
-			for(int j = 0; j < matrixSize; j++) {
-				cout << nodeMatrix[i][j] << " ";
-			}
-			cout << endl;
+	// print matrix 
+	for(int i = 0; i < matrixSize; i++) {
+		for(int j = 0; j < matrixSize; j++) {
+			cout << nodeMatrix[i][j] << " ";
 		}
 		cout << endl;
-		// end print matrix 
 	}
+	cout << endl;
+	// end print matrix 
+
 	*/
-}
 
 	return 0;
 }
